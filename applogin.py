fea3b0fe4dad9f4b0b6937f9cf82a00b76df4c6a
pfbrodon <pfbrodon@gmail.com>
2023-11-29 11:22:34 -0300

    nueva

--- applogin.py
+++ applogin.py
@@ -1,17 +1,19 @@
 from flask import Flask  ,request, render_template, Response, session, redirect
 # del modulo flask importar la clase Flask y los métodos jsonify,request
-from flask_mysqldb import MySQL, MySQLdb
+#from flask_mysqldb import MySQL, MySQLdb
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app=Flask(__name__, template_folder='template')  # crear el objeto app de la clase Flask
+CORS(app) #modulo cors es para que me permita acceder desde el frontend al backend
 
 ###########################################################################
-app.config['MYSQL_HOST']='pfbrodon.mysql.pythonanywhere-services.com'
-app.config['MYSQL_USER']='pfbrodon'
-app.config['MYSQL_PASSWORD']='prueba1234'
-app.config['MYSQL_DB']='pfbrodon$proyecto'
-app.config['MYSQL_CURSORCLASS']='DictCursor'
+#app.config['MYSQL_HOST']='pfbrodon.mysql.pythonanywhere-services.com'
+#app.config['MYSQL_USER']='pfbrodon'
+#app.config['MYSQL_PASSWORD']='prueba1234'
+#app.config['MYSQL_DB']='pfbrodon$proyecto'
+#app.config['MYSQL_CURSORCLASS']='DictCursor'
 ############################################################################
 #app.config['MYSQL_HOST']='localhost'
 #app.config['MYSQL_USER']='root'
@@ -20,13 +22,13 @@ app.config['MYSQL_CURSORCLASS']='DictCursor'
 #app.config['MYSQL_CURSORCLASS']='DictCursor'
 #############################################################################
 ## configuro la base de datos, con el nombre el usuario y la clave
-#app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://pfbrodon:prueba1234@pfbrodon.mysql.pythonanywhere-services.com/pfbrodon$proyecto'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://pfbrodon:prueba1234@pfbrodon.mysql.pythonanywhere-services.com/pfbrodon$proyecto'
 ## URI de la BBDD                          driver de la BD  user:clave@URLBBDD/nombreBBDD
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
-#db= SQLAlchemy(app)   #crea el objeto db de la clase SQLAlquemy
-#ma=Marshmallow(app)   #crea el objeto ma de de la clase Marshmallow
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
+db= SQLAlchemy(app)   #crea el objeto db de la clase SQLAlquemy
+ma=Marshmallow(app)   #crea el objeto ma de de la clase Marshmallow
 #############################################################################
-mysql=MySQL(app)
+#mysql=MySQL(app)
 
 @app.route('/')
 def home():
@@ -36,14 +38,17 @@ def home():
 def admin():
     return render_template('admin.html')
 
+class Login(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(45), unique=True, nullable=False)
+    password = db.Column(db.String(8), nullable=False)
 
 #FUNCION LOGIN
-@app.route('/acceso-login', methods =["GET","POST"])
+@app.route('/login', methods =["GET","POST"])
 def login():
     if request.method =='POST' and 'txtCorreo' in request.form and 'txtPassword':
         _correo = request.form['txtCorreo']
         _password = request.form['txtPassword']
-        cur=mysql.connection.cursor()
         cur.execute('SELECT * FROM login WHERE email = %s AND password = %s',(_correo,_password,))
         account= cur.fetchone()
         if account:
@@ -54,5 +59,4 @@ def login():
             return render_template('index.html', mensaje="Usuario Incorrecto")
         
 if __name__=='__main__':
-    app.secret_key="adminbuloneria"
     app.run(debug=True, port=5000, threaded=True)
--- template/INSERT INTO login (email,password) VALUE.sql
+++ template/INSERT INTO login (email,password) VALUE.sql
@@ -1,1 +1,2 @@
 INSERT INTO login (email,password) VALUES ('usuario@email.com','123456');
+INSERT INTO login (email,password) VALUES ('pfbrodon@gmail.com','123456');
